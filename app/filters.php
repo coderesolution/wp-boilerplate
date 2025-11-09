76f57517bc895e487d2fd043671c76591f3cf17e
Elliott Mangham <elliott@coderesolution.com>
2025-11-09 11:50:44 +0000

    ✨ Add SVG upload support with security validation

--- app/filters.php
+++ app/filters.php
@@ -1,10 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Theme filters.
  */
 
 namespace App;
+
+/**
+ * Load Roots Allow SVG plugin.
+ *
+ * Enables SVG uploads with security validation.
+ * @link https://github.com/roots/allow-svg
+ */
+if (file_exists(__DIR__ . '/../vendor/roots/allow-svg/allow-svg.php')) {
+    require_once __DIR__ . '/../vendor/roots/allow-svg/allow-svg.php';
+}
 
 /**
  * Add "… Continued" to the excerpt.
